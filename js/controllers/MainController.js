6332bb0b7b7b75828574b595052e0605388bfa9a
Thomas Santos <thomas@socialeffort.co>
2017-10-24 05:55:57 -0400

    add plural cars object

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,10 +1,22 @@
 app.controller('MainController', ['$scope', function($scope) {
     $scope.title = 'Supercars of 2017';
     $scope.promo = '';
-    $scope.product = 
-    {
-        name: 'Lamborghini Huracan', 
-        color: 'Blaze Red',
-        price: '199800'
-    }
-  }]);
+    $scope.products = 
+[
+{
+    name: 'Lamborghini Huracan', 
+    color: 'Blaze Red',
+    price: '199800'
+},
+{
+    name: 'Bugatti Chiron', 
+    color: 'Nero',
+    price: '2500000'
+},
+{
+    name: 'Ferrari Enzo', 
+    color: 'Crimson',
+    price: '659330'
+}]
+
+}]);
