4fc34b4d46ea54dbdc5cceb5c95aefc5a3e70ab8
Thomas Santos <thomas@socialeffort.co>
2017-10-24 05:37:50 -0400

    add scope title, promo, product properties

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,4 +1,10 @@
 app.controller('MainController', ['$scope', function($scope) {
-    $scope.title = 'Books of the Year';
-    $scope.promo = 'Can you read them all?';
+    $scope.title = 'Supercars of 2017';
+    $scope.promo = '';
+    $scope.product = 
+    {
+        name: 'Lamborghini Huracan', 
+        color: 'Blaze Red',
+        price: '$199800'
+    }
   }]);
