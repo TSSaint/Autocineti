940b0935b9a022c3ae05c3ca7085fb3e515bccd3
Thomas Santos <thomas@socialeffort.co>
2017-10-24 06:16:49 -0400

    change content

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -6,22 +6,26 @@ app.controller('MainController', ['$scope', function($scope) {
 {
     name: 'Lamborghini Huracan', 
     color: 'Blaze Red',
-    price: '199800'
+    price: '199800',
+    cover: 'img/huracan.jpg'
 },
 {
     name: 'Bugatti Chiron', 
     color: 'Nero',
-    price: '2500000'
+    price: '2500000',
+    cover: 'img/chiron.jpg'
 },
 {
     name: 'Ferrari Enzo', 
     color: 'Crimson',
-    price: '1999999'
+    price: '1999999',
+    cover: 'img/enzo.jpg'
 },
 {
     name: 'Ferrari F80 (Concept)', 
     color: 'Ferrari Red',
-    price: '9999999'
+    price: '9999999',
+    cover: 'img/f80.jpg'
 }]
 
 }]);
