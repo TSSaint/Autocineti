9d119607492b19598b0df61d5debf6977b25014b
Thomas Santos <thomas@socialeffort.co>
2017-10-24 20:21:04 -0400

    add content in object

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,5 +1,5 @@
 app.controller('MainController', ['$scope', function($scope) {
-    $scope.title = 'Supercars of 2017';
+    $scope.title = 'Supercars';
     $scope.promo = '';
     $scope.products = 
 [
@@ -23,6 +23,72 @@ app.controller('MainController', ['$scope', function($scope) {
 },
 {
     name: 'Ferrari F80 (Concept)', 
+    color: 'Ferrari Red',
+    price: '9999999',
+    cover: 'img/f80.jpg'
+},
+{
+    name: 'Acura NSX', 
+    color: 'Nero',
+    price: '2500000',
+    cover: 'img/chiron.jpg'
+},
+{
+    name: 'Pagani Zonda', 
+    color: 'Crimson',
+    price: '1999999',
+    cover: 'img/enzo.jpg'
+},
+{
+    name: 'Koenigsegg One:1', 
+    color: 'Ferrari Red',
+    price: '9999999',
+    cover: 'img/f80.jpg'
+},
+{
+    name: 'Aston Martin One-77', 
+    color: 'Nero',
+    price: '2500000',
+    cover: 'img/chiron.jpg'
+},
+{
+    name: 'Venom GT', 
+    color: 'Crimson',
+    price: '1200000',
+    cover: 'img/enzo.jpg'
+},
+{
+    name: 'McLaren P1', 
+    color: 'Ferrari Red',
+    price: '9999999',
+    cover: 'img/f80.jpg'
+},
+{
+    name: 'Zenvo ST1', 
+    color: 'Ferrari Red',
+    price: '9999999',
+    cover: 'img/f80.jpg'
+},
+{
+    name: 'Lamborghini Reventon', 
+    color: 'Crimson',
+    price: '1200000',
+    cover: 'img/enzo.jpg'
+},
+{
+    name: 'Ferrari LaFerrari', 
+    color: 'Ferrari Red',
+    price: '9999999',
+    cover: 'img/f80.jpg'
+},
+{
+    name: 'W Motors Lykan Hypersport', 
+    color: 'Ferrari Red',
+    price: '9999999',
+    cover: 'img/f80.jpg'
+},
+{
+    name: 'Lamborghini Veneno', 
     color: 'Ferrari Red',
     price: '9999999',
     cover: 'img/f80.jpg'
